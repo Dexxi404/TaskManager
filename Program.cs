1e689025ecd085eed38b80b0c4b02921e7d0f07b
rajan <rajandeepak836@gmail.com>
2026-04-16 21:08:10 +0530

    set up completed i guess

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<TaskManagerContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
